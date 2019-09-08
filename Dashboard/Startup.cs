a853b903188fab1382f52a21cb1c7f3ce2bdcaad
solly <mail4solly@gmail.com>
2019-09-09 02:54:11 +1200

    Startup update

--- Dashboard/Startup.cs
+++ Dashboard/Startup.cs
@@ -1,9 +1,19 @@
+using Dashboard.Extensions;
+using Dashboard.Interfaces;
+using Dashboard.Models;
+using Dashboard.Models.JsonConverters;
+using Dashboard.Models.Mappers;
+using Dashboard.Models.Warnings;
+using Dashboard.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Dashboard
 {
@@ -19,7 +29,56 @@ namespace Dashboard
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<DashboardContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DashboardContext")));
+            services.AddSingleton(new TimeZoneSettings
+            {
+                Id = Configuration.GetValue<string>("TimeZoneId")
+            });
+            services.AddSingleton(Configuration.GetSection("BranchFinder").Get<BranchFinderSettings>());
+            services.AddSingleton(Configuration.GetSection("AdminApi").Get<AdminApiSettings>());
+            services.AddSingleton<IWarning, StockOnHandWarning>();
+            services.AddSingleton<IWarning, BackOrderWarning>();
+            services.AddSingleton<IWarning, SubstitutionWarning>();
+            services.AddSingleton<IWarning, OnHoldWarning>();
+            services.AddSingleton<IWarning, ParkedWarning>();
+            services.AddSingleton<IWarning, DeliveryEtaWarning>();
+            services.AddSingleton<WarningTester>();
+            services.AddSingleton<IMapper, GridRowMapper>();
+            services.AddSingleton<IMapper, NationalOrderStatusMapper>();
+            services.AddSingleton<DeliveryOrdersMapper>();
+            services.AddScoped<IDeliveryOrderRepository, DeliveryOrderRepository>();
+            services.AddScoped<IStatisticsRepository, StatisticsRepository>();
+            services.AddScoped<IDashboardUserRepository, DashboardUserRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddSingleton<ISourceRepository, InMemorySourceRepository>();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                    options.SerializerSettings.Converters.Add(new PickerConverter());
+                });
+            //services
+            //    .AddAuthentication(options =>
+            //    {
+            //        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            //    })
+            //    .AddCookie()
+            //    .AddOpenIdConnect(options =>
+            //    {
+            //        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //        options.ClientId = Configuration.GetValue<string>("ADFS:ClientId");
+            //        options.Authority = Configuration.GetValue<string>("ADFS:Authority");
+            //        options.CallbackPath = new PathString("/signin-oidc");
+            //        options.Scope.Add("email");
+            //        options.GetClaimsFromUserInfoEndpoint = true;
+            //        options.Events.OnTicketReceived = async context =>
+            //        {
+            //            await context.HttpContext.RequestServices.GetRequiredService<IDashboardUserRepository>().GetAdditionalClaims(context.Principal.Identity as ClaimsIdentity);
+            //        };
+            //    });
 
             // Register the Swagger services
             services.AddSwaggerDocument();
